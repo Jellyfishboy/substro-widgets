e909b65006a6ab6d5dc28d38e50935647d81f775
Tom Dallimore <me@tomdallimore.com>
2024-05-18 05:55:17 +0700

    Set up initial project and added tile widget design.

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+export default {
+  build: {
+    rollupOptions: {
+      input: {
+        home: "index.html",
+        tile: "tile.html",
+        column: "column.html",
+        loyalty: "loyalty.html",
+      },
+    },
+  },
+};
